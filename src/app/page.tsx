d032177cbf88eea9b3479c11d4d27a38f685a212
harindu-pe <harindu.pe@gmail.com>
2025-05-04 17:33:16 +0530

    customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+
 export default function Home() {
-  return <div>hello world</div>;
+  return (
+    <div>
+      <div className="flex flex-col gap-y-4 p-4 max-w-md">
+        <div>
+          <Button variant="elevated">Button</Button>
+        </div>
+        <Input placeholder="enter text" />
+        <Progress value={50} />
+        <Textarea placeholder="I am textarea" />
+        <Checkbox />
+      </div>
+    </div>
+  );
 }
